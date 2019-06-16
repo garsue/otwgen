fb95f6cef2197149ded95e697a44fa9425c8cc52
garsue <osuga.shunya@mfkessai.co.jp>
2019-06-17 01:30:38 +0900

    Refactoring

--- parse.go
+++ parse.go
@@ -1,6 +1,7 @@
 package otwrapper
 
 import (
+	"fmt"
 	"go/ast"
 	"go/format"
 	"go/token"
@@ -13,84 +14,106 @@ import (
 func Parse(pkgs []*packages.Package) {
 	for _, pkg := range pkgs {
 		for _, file := range pkg.Syntax {
-			out := &ast.File{
-				Name: ast.NewIdent(pkg.Name),
-				Decls: []ast.Decl{
-					&ast.GenDecl{
-						Tok: token.IMPORT,
-						Specs: []ast.Spec{
-							&ast.ImportSpec{
-								Path: &ast.BasicLit{
-									Kind:  token.STRING,
-									Value: strconv.Quote(pkg.ID),
-								},
-							},
-						},
-					},
-				},
-			}
+			out := NewFile(pkg)
 
+			var found bool
 			ast.Inspect(file, func(n ast.Node) bool {
-				fdecl, ok := n.(*ast.FuncDecl)
-				if !ok {
+				switch decl := n.(type) {
+				case *ast.FuncDecl:
+					if !decl.Name.IsExported() {
+						return true
+					}
+					out.Decls = append(out.Decls, NewFunc(decl, pkg.Name))
+
+				default:
 					return true
 				}
-				if !fdecl.Name.IsExported() {
-					return true
-				}
-
-				// Function
-				if fdecl.Recv == nil {
-					args := make([]ast.Expr, 0, fdecl.Type.Params.NumFields())
-					for _, field := range fdecl.Type.Params.List {
-						for _, name := range field.Names {
-							args = append(args, name)
-						}
-					}
-
-					var stmt ast.Stmt
-					if fdecl.Type.Results.NumFields() == 0 {
-						stmt = &ast.ExprStmt{
-							X: &ast.CallExpr{
-								Fun: &ast.SelectorExpr{
-									X:   ast.NewIdent(pkg.Name),
-									Sel: fdecl.Name,
-								},
-								Args: args,
-							},
-						}
-					} else {
-						stmt = &ast.ReturnStmt{
-							Results: []ast.Expr{
-								&ast.CallExpr{
-									Fun: &ast.SelectorExpr{
-										X:   ast.NewIdent(pkg.Name),
-										Sel: fdecl.Name,
-									},
-									Args: args,
-								},
-							},
-						}
-					}
-
-					out.Decls = append(out.Decls, &ast.FuncDecl{
-						Doc:  fdecl.Doc,
-						Name: fdecl.Name,
-						Type: fdecl.Type,
-						Body: &ast.BlockStmt{
-							List: []ast.Stmt{
-								stmt,
-							},
-						},
-					})
-				}
+				found = true
 
 				return true
 			})
 
+			if !found {
+				continue
+			}
+
 			if err := format.Node(os.Stdout, token.NewFileSet(), out); err != nil {
 				panic(err)
 			}
+			fmt.Println("==================")
 		}
+	}
+}
+
+func NewFunc(fdecl *ast.FuncDecl, pkgName string) *ast.FuncDecl {
+	// Function
+	if fdecl.Recv == nil {
+		return &ast.FuncDecl{
+			Doc:  fdecl.Doc,
+			Name: fdecl.Name,
+			Type: fdecl.Type,
+			Body: &ast.BlockStmt{
+				List: []ast.Stmt{
+					NewFuncBody(fdecl, pkgName),
+				},
+			},
+		}
+	}
+	// Method
+	return &ast.FuncDecl{
+		Doc:  fdecl.Doc,
+		Recv: fdecl.Recv,
+		Name: fdecl.Name,
+		Type: fdecl.Type,
+		Body: &ast.BlockStmt{
+			List: []ast.Stmt{
+				NewFuncBody(fdecl, pkgName),
+			},
+		},
+	}
+}
+
+func NewFuncBody(fdecl *ast.FuncDecl, pkgName string) ast.Stmt {
+	args := make([]ast.Expr, 0, fdecl.Type.Params.NumFields())
+	for _, field := range fdecl.Type.Params.List {
+		for _, name := range field.Names {
+			args = append(args, name)
+		}
+	}
+	expr := ast.CallExpr{
+		Fun: &ast.SelectorExpr{
+			X:   ast.NewIdent(pkgName),
+			Sel: fdecl.Name,
+		},
+		Args: args,
+	}
+	if fdecl.Type.Results.NumFields() == 0 {
+		return &ast.ExprStmt{
+			X: &expr,
+		}
+	}
+	return &ast.ReturnStmt{
+		Results: []ast.Expr{
+			&expr,
+		},
+	}
+}
+
+func NewFile(pkg *packages.Package) *ast.File {
+	return &ast.File{
+		Name: ast.NewIdent(pkg.Name),
+		Decls: []ast.Decl{
+			&ast.GenDecl{
+				Tok: token.IMPORT,
+				Specs: []ast.Spec{
+					&ast.ImportSpec{
+						Path: &ast.BasicLit{
+							Kind:  token.STRING,
+							Value: strconv.Quote(pkg.ID),
+						},
+					},
+				},
+			},
+		},
 	}
 }
