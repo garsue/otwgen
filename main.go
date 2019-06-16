06cabef57f579c5a8d4b6e125c2d76e34fda8df9
garsue <osuga.shunya@mfkessai.co.jp>
2019-06-16 18:01:30 +0900

    Parse pkg

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"go/ast"
 	"os"
 
 	"golang.org/x/tools/go/packages"
@@ -13,7 +14,7 @@ func main() {
 	flag.StringVar(&pattern, "pattern", "", "package pattern")
 	flag.Parse()
 
-	cfg := &packages.Config{Mode: packages.NeedFiles | packages.NeedSyntax}
+	cfg := &packages.Config{Mode: packages.NeedSyntax | packages.NeedDeps | packages.NeedTypes}
 	patterns := make([]string, 0, 1)
 	if pattern != "" {
 		patterns = append(patterns, pattern)
@@ -32,6 +33,21 @@ func main() {
 	// Print the names of the source files
 	// for each package listed on the command line.
 	for _, pkg := range pkgs {
-		fmt.Println(pkg.ID, pkg.GoFiles)
+		for _, s := range pkg.Syntax {
+			//log.Println(s)
+			if err := ast.Print(pkg.Fset, s); err != nil {
+				panic(err)
+			}
+		}
+
+		//for _, p := range pkg.GoFiles {
+		//	f, err := parser.ParseFile(pkg.Fset, p, nil, 0)
+		//	if err != nil {
+		//		log.Panic(err)
+		//	}
+		//	if err := ast.Print(pkg.Fset, f); err != nil {
+		//		log.Panic(err)
+		//	}
+		//}
 	}
 }
